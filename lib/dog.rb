f683a89010f0f1bc72fcf082c0c23be105b8ebe9
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-26 22:55:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,13 @@ class Dog
     end
 
     def name=(dogs_name)
-    @this_dogs_name = dogs_name
+      @this_dogs_name = dogs_name
     end
 
-    def name
-    @this_dogs_name
-  end
+   def name
+      @this_dogs_name
+   end
 end
-
-end# Your code goes here!
 
 fido = Dog.new
 fido.name = "Fido"
